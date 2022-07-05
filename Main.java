aefee4769f2eeff15c060554fae55031f0051eb6
Shubham Rawat <46570973+shubhamrawat140798@users.noreply.github.com>
2022-07-05 11:57:01 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,34 @@
+/*new fixed thread */
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+class TestThread {
+	
+   public static void main(final String[] arguments) throws InterruptedException {
+      ExecutorService executor = Executors.newFixedThreadPool(2);
+      ThreadPoolExecutor pool = (ThreadPoolExecutor) executor;
+      executor.submit(new Task());
+      executor.submit(new Task());
+      executor.shutdown();
+   }  
+
+   static class Task implements Runnable {
+
+      public void run() {
+         
+         try {
+            Long dur = (long) (Math.random() * 5);
+            System.out.println("Running Task! Thread Name: " +
+               Thread.currentThread().getName());
+               TimeUnit.SECONDS.sleep(dur);
+            
+            System.out.println("Task Completed! Thread Name: " +
+               Thread.currentThread().getName());
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+      }
+   }
+   }
